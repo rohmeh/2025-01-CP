2a207cb0750b71c89f634c6189658cd87ade0af6
prafullkumartale <prafullkumar@iiserpune.ac.in>
2025-01-16 14:41:33 +0530

    Added two problems for Lecture 04

---
+++ test-07-20001.cpp
@@ -0,0 +1,32 @@
+/*
+Name: Permutations
+Reference: https://cses.fi/problemset/task/1070
+
+A permutation of integers 1,2,\ldots,n is called beautiful if 
+there are no adjacent elements whose difference is 1.
+
+Input: There are t test cases. In each test case, you are given an intger n.
+If there are several solutions, you may print any of them.
+
+Output: For each case, print the a beautiful permutation if such a permutation exists.
+Otherwise print -1.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {    
+    int t;
+    cin >> t;   
+    long long sum_missing_nrs = 0;
+    
+    /*
+
+    Code is not complete
+    */
+    
+    //Critical line
+    cout << "20001\t" << "Donald Knuth\t" << sum_missing_nrs <<"\n";
+    return 0;
+}
