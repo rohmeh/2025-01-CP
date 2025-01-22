6d3f09d462f690a26075430d10836b7f966696a5
Jay Bharambe <112482222+jayb71@users.noreply.github.com>
2025-01-22 17:49:04 +0530

    test-06: updated solution

--- test-06-20001.cpp
+++ test-06-20001.cpp
@@ -8,21 +8,39 @@ Your task is to find the longest repetition in the sequence.
 This is a maximum-length substring containing only one type of character.
 
 Output: Print the sum of maximum-length of substring in each case.
+Contributer: Jay Bharambe 22159
+git: jayb71
 */
+
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
+int calculate_max_repetition(string dnaSequence) {
+    int currentCount = 1;
+    int maxCount = 1;
+    for(int i=1; i<dnaSequence.length(); i++) {
+        if (dnaSequence[i]==dnaSequence[i-1]) {
+            currentCount++;
+        } else {
+           maxCount = max(currentCount, maxCount);
+           currentCount = 1;
+        }
+    }
+    return max(maxCount, currentCount);
+}
+
+
 int main() {    
-    int t;
-    cin >> t;   
     long long sum_missing_nrs = 0;
     
-    /*
-
-    Code is not complete
-    */
+    string dnaSequence;
+    cin >> dnaSequence;
+    
+    
+    sum_missing_nrs = calculate_max_repetition(dnaSequence);
+    
     
     //Critical line
     cout << "20001\t" << "Donald Knuth\t" << sum_missing_nrs <<"\n";
